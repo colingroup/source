1d652406c572ef786547c083cf2d5af821dbd34d
Pedro Alonso <devfindsingles@gmail.com>
2020-06-05 18:23:11 +0200

    First

---
+++ _include/config/db.php
@@ -0,0 +1,5 @@
+<?php
+        $g['db']['host'] = stripslashes('127.0.0.1');
+        $g['db']['db'] = stripslashes('findsingles_onli');
+        $g['db']['user'] = stripslashes('findsingles_onli');
+        $g['db']['password'] = stripslashes('saidsalim210953');
