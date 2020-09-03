6e614690cd62390706af14055d366715788808b3
anaconda0905 <conda0905@gmail.com>
2020-09-03 13:46:01 +0200

    ios svg icon added

--- _include/current/vk.class.php
+++ _include/current/vk.class.php
@@ -3,26 +3,26 @@
 class vk
 {
     private static $instance;
-    private static $nameSocial='vk';
+    private static $nameSocial = 'vk';
 
     private function getUrlAuth()
     {
-      return 'social_login.php?module='.self::$nameSocial;
+        return 'social_login.php?module=' . self::$nameSocial;
     }
 
     private function getUrlToken()
     {
-        return  '';
+        return '';
     }
 
     public function getParamsAuth()
     {
-        $client_id = Common::getOption(self::$nameSocial.'_appid');
-        $client_secret = Common::getOption(self::$nameSocial.'_secret');
+        $client_id = Common::getOption(self::$nameSocial . '_appid');
+        $client_secret = Common::getOption(self::$nameSocial . '_secret');
 
         $params = array(
             'response_type' => 'code',
-            'client_id'     => $client_id,
+            'client_id' => $client_id,
             'client_secret' => $client_secret,
             'scope' => 'email'
         );
@@ -33,9 +33,9 @@ class vk
     public function getUserInfo($code)
     {
         $result = false;
-        $userInfo=false;
+        $userInfo = false;
 
-        $userInfo=get_session(self::$nameSocial.'_user_info',false);
+        $userInfo = get_session(self::$nameSocial . '_user_info', false);
 
         return $userInfo;
     }
@@ -44,23 +44,23 @@ class vk
     public static function getInstance()
     {
         global $g;
-        if (isset($g['options'][self::$nameSocial.'_appid'])
-            && isset($g['options'][self::$nameSocial.'_secret'])
-            && $g['options'][self::$nameSocial.'_appid'] != ''
-            && $g['options'][self::$nameSocial.'_secret'] != ''
+        if (isset($g['options'][self::$nameSocial . '_appid'])
+            && isset($g['options'][self::$nameSocial . '_secret'])
+            && $g['options'][self::$nameSocial . '_appid'] != ''
+            && $g['options'][self::$nameSocial . '_secret'] != ''
         ) {
 
-            if(self::$instance === null){
+            if (self::$instance === null) {
                 // Create our Application instance (replace this with your appId and secret)
                 self::$instance = new self(array(
-                    'appId' => $g['options'][self::$nameSocial.'_appid'],
-                    'secret' => $g['options'][self::$nameSocial.'_secret'],
+                    'appId' => $g['options'][self::$nameSocial . '_appid'],
+                    'secret' => $g['options'][self::$nameSocial . '_secret'],
                     'cookie' => true,
                 ));
 
             }
 
-            return  self::$instance;
+            return self::$instance;
         } else {
             return false;
         }
@@ -73,9 +73,9 @@ class vk
 
     public function getUserId()
     {
-        $userInfo = get_session(self::$nameSocial.'_user_info');
+        $userInfo = get_session(self::$nameSocial . '_user_info');
 
-        if(isset($userInfo['id'])){
+        if (isset($userInfo['id'])) {
             return $userInfo['id'];
         } else {
             return false;
@@ -94,10 +94,10 @@ class vk
         global $g;
 
 
-        $me = get_session(self::$nameSocial.'_user_info');
+        $me = get_session(self::$nameSocial . '_user_info');
 
-        set_session(self::$nameSocial.'_id', 0);
-        set_session(self::$nameSocial.'_photo', false);
+        set_session(self::$nameSocial . '_id', 0);
+        set_session(self::$nameSocial . '_photo', false);
         set_session('social_id', 0);
         set_session('social_photo', false);
 
@@ -111,65 +111,65 @@ class vk
                 }
             }
 
-            if(!isset($me['first_name'])){
-                $me['first_name']='';
+            if (!isset($me['first_name'])) {
+                $me['first_name'] = '';
             }
 
-            if(!isset($me['last_name'])){
-                $me['last_name']='';
+            if (!isset($me['last_name'])) {
+                $me['last_name'] = '';
             }
 
             if (isset($me['first_name'])) {
                 if (get_param('join_handle') == '') {
-                    $_GET['join_handle'] = implode(' ',array($me['first_name'],$me['last_name']));
+                    $_GET['join_handle'] = implode(' ', array($me['first_name'], $me['last_name']));
                 }
             }
 
-                if (isset($me['bdate']) && $me['bdate']) {
-                    $birthDate = explode('.', $me['bdate']);
+            if (isset($me['bdate']) && $me['bdate']) {
+                $birthDate = explode('.', $me['bdate']);
 
-                    if (is_array($birthDate) && count($birthDate)) {
-                        if (get_param('month') == '' && isset($birthDate[1])) {
-                            $_GET['month'] = $birthDate[1];
-                        }
-                        if (get_param('day') == '' && isset($birthDate[0])) {
-                            $_GET['day'] = $birthDate[0];
-                        }
-                        if (get_param('year') == '' && isset($birthDate[2])) {
-                            $_GET['year'] = $birthDate[2];
-                        }
+                if (is_array($birthDate) && count($birthDate)) {
+                    if (get_param('month') == '' && isset($birthDate[1])) {
+                        $_GET['month'] = $birthDate[1];
+                    }
+                    if (get_param('day') == '' && isset($birthDate[0])) {
+                        $_GET['day'] = $birthDate[0];
+                    }
+                    if (get_param('year') == '' && isset($birthDate[2])) {
+                        $_GET['year'] = $birthDate[2];
                     }
                 }
+            }
 
 
-            set_session(self::$nameSocial.'_id', $me['id']);
+            set_session(self::$nameSocial . '_id', $me['id']);
             set_session('social_id', $me['id']);
             set_session('social_type', self::$nameSocial);
             // set picture if exists
-            if(isset($me['photo_big']) && strpos($me['photo_big'],'camera_200.png')===false) {
+            if (isset($me['photo_big']) && strpos($me['photo_big'], 'camera_200.png') === false) {
                 set_session('social_photo', $me['photo_big']);
             } else {
                 set_session('social_photo', '');
             }
 
-                    if(isset($me['sex'])) {
-                        $gender = $me['sex'];
-                        if($gender == 1) {
-                            $gender = 'F';
-                        }
-                        if($gender == 2) {
-                            $gender = 'M';
-                        }
+            if (isset($me['sex'])) {
+                $gender = $me['sex'];
+                if ($gender == 1) {
+                    $gender = 'F';
+                }
+                if ($gender == 2) {
+                    $gender = 'M';
+                }
 
-                        $sql = 'SELECT id FROM const_orientation
+                $sql = 'SELECT id FROM const_orientation
                             WHERE gender = ' . to_sql($gender, 'Text') . '
                             ORDER BY id ASC LIMIT 1';
-                        $orientation = DB::result($sql);
+                $orientation = DB::result($sql);
 
-                        if($orientation) {
-                            $_GET['orientation'] = $orientation;
-                        }
-                    }
+                if ($orientation) {
+                    $_GET['orientation'] = $orientation;
+                }
+            }
 
         }
     }
@@ -189,28 +189,29 @@ class vk
 
     public function oAuthApi()
     {
-        $nameSocial=self::$nameSocial;
-        $params=$this->getParamsAuth();
+        die("here");
+        $nameSocial = self::$nameSocial;
+        $params = $this->getParamsAuth();
         $currentUrl = Common::urlSite() . $this->loginRedirectUrl();
         $email = '';
 
         if (isset($_GET['code'])) {
             $result = false;
-            $params['redirect_uri']=$currentUrl;
+            $params['redirect_uri'] = $currentUrl;
             $params['code'] = $_GET['code'];
             $params['scope'] = 'email';
             $token = json_decode(urlGetContents('https://oauth.vk.com/access_token', 'get', $params), true);
 
             if (isset($token['access_token'])) {
                 $params = array(
-                    'uids'         => $token['user_id'],
-                    'fields'       => 'id,first_name,last_name,screen_name,sex,bdate,photo_big',
+                    'uids' => $token['user_id'],
+                    'fields' => 'id,first_name,last_name,screen_name,sex,bdate,photo_big',
                     'access_token' => $token['access_token'],
                     'scope' => 'email',
                     'v' => '5.0',
                 );
-                
-                if(isset($token['email'])){
+
+                if (isset($token['email'])) {
                     $email = $token['email'];
                 }
 
@@ -231,15 +232,14 @@ class vk
         }
 
 
-
         $url = 'http://oauth.vk.com/authorize';
 
-            $params2 = array(
-                'client_id'     => $params['client_id'],
-                'redirect_uri'  => $currentUrl,
-                'response_type' => 'code',
-                'scope' => 'email'
-            );
+        $params2 = array(
+            'client_id' => $params['client_id'],
+            'redirect_uri' => $currentUrl,
+            'response_type' => 'code',
+            'scope' => 'email'
+        );
 
         redirect($url . '?' . http_build_query($params2));
 
